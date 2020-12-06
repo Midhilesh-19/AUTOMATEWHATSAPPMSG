f9117a03095296307666110242effb0165860502
Midhilesh <72137078+Midhilesh-19@users.noreply.github.com>
2020-12-06 00:44:01 -0800

    Update autowhatsappbot.py

--- autowhatsappbot.py
+++ autowhatsappbot.py
@@ -1,3 +1,3 @@
-import pywhatkit
+import pywhatkit # here the pywhatkit is pyhton module which we will use to access our whatsaap and send message 
 pywhatkit.sendwhatmsg(
-    '+919866759282', 'dad its me', 13, 50)
+    '+9198********', 'happy birthday baby', 24, 00) #her ewe have to give tym in a 24 hours format
